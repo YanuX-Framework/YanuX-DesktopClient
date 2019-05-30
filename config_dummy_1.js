88d1d7ccf40ef242f1c21ba556635f362110a2ac
petersaints <petersaints@gmail.com>
2019-05-30 19:37:51 +0100

    - Dummy Configs

--- config_dummy_1.js
+++ config_dummy_1.js
@@ -1,12 +1,12 @@
 {
-    "http_server_port": 3003,
+    "http_server_port": 3013,
     "allow_zeroconf": false,
     "client_id": "yanux-desktop-client",
     "client_secret": "topsecret_client_secret",
     "redirect_uri": "http://localhost:3003/",
     "broker_url": "http://localhost:3002/",
     "oauth2_authorization_server_url": "http://localhost:3001/",
-    "beacon_scan": false,
+    "beacon_scan": true,
     "beacon_advertise": false,
     "beacon_matcher_parameters": [
         null,
@@ -18,17 +18,17 @@
     "beacon_advertiser_parameters": [
         "113069EC-6E64-4BD3-6810-DE01B36E8A3E",
         1,
-        100
+        101
     ],
     "beacons_print_updated": false,
     "beacons_print_cleared": false,
     "beacons_clear_console": false,
     "beacons_refresh_interval": 500,
     "beacons_inactivity_timer": 1000,
-    "device_uuid": "9ab8e750-bc1e-11e8-a769-3f2e91eebf08",
+    "device_uuid": "9ab8e750-bc1e-11e8-a769-3f2e91eebf09",
     "device_capabilities": {
-        "view": true,
-        "control": false
+        "view": false,
+        "control": true
     },
     "access_token": "tyDUPb4BSdjKGhATgPfEmmvdZRu0HmEfcOVoLUQr0JbNoo4mVjCb3ChdU7bI20k0Gfg6sP9Lol6Q8t1MjH9LFvX0rE9zK1vAMq2XiEaklFtHRWVO8JgeLo7seJZVrNvl8pDMyTK8lCRH3BmbqmrabTM5zN62AlsIOvbglKgCvCR64m5FCJlHFG0hHjKAsuXTsSQ2MqTy61t899UDDaWfn4GvC3Sa51wtGyWN2FpBlUKBXn2HDCn9ovK31B1PPAuc",
     "refresh_token": "l3LU6ZjIMDaYEBPpgAfEF7Iydzoi9UVl07LTgSpUGqjataifJ1FE2bH5Zex3AODBQjLoRHieOt07PLYT2RsaamOrvkbFR9Ysy5moammSWoBap7kLKnqlcOi7M6FbOdl0tNPOhZinMWvom1FVztT1os2HUXcHZbiGBXWwTHFYH8EkdhdYJpcBuh6bCz9Nibm8RoSeTgnRQPbfCtwht8dq602HhuaujbG2XrPl0zTwQQMz4NcDQTvgA1fR6d3sRwOE"
